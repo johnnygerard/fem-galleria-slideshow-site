ac7e3242002041456805c8b47c8ee14e2a0601f3
Johnny Gérard <john@jgerard.dev>
2024-01-29 21:30:29 +0100

    feat: log attribution and portfolio link

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  console.log('Challenge by Frontend Mentor. Coded by Johnny Gérard.');
+  console.log('Portfolio: https://www.frontendmentor.io/profile/johnnygerard');
+}
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
