b1b69c5b1dd8eedfa7654ca09f06f8dc12eef2f1
Johnny Gérard <john@jgerard.dev>
2024-02-03 02:02:10 +0100

    feat: add navigation on arrow keys

--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -1,9 +1,10 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, Input } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { SlideshowBodyComponent } from '../slideshow-body/slideshow-body.component';
 import { SlideshowFooterComponent } from '../slideshow-footer/slideshow-footer.component';
 import { Painting } from '../../types/painting.class';
 import { paintings } from '../paintings';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-slideshow',
@@ -32,5 +33,28 @@ export class SlideshowComponent {
       top: 0,
       behavior: 'smooth'
     });
+  }
+
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute,
+  ) { }
+
+  @HostListener('body:keydown.ArrowLeft')
+  navigateToPreviousPainting() {
+    const previous = this.painting.previous?.slug;
+
+    if (previous) this.#navigateToPainting(previous);
+  }
+
+  @HostListener('body:keydown.ArrowRight')
+  navigateToNextPainting() {
+    const next = this.painting.next?.slug;
+
+    if (next) this.#navigateToPainting(next);
+  }
+
+  #navigateToPainting(slug: string) {
+    this._router.navigate(['..', slug], { relativeTo: this._route });
   }
 }
