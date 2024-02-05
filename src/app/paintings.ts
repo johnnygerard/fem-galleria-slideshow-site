38c822918e33423a415d325037c7f908afb1faee
Johnny Gérard <john@jgerard.dev>
2024-02-05 12:23:49 +0100

    feat: update painting title

--- src/app/paintings.ts
+++ src/app/paintings.ts
@@ -9,7 +9,7 @@ const data: ConstructorParameters<typeof Painting>[] = [
     "Vincent Van Gogh",
     "Although The Starry Night was painted during the day in Van Gogh's ground-floor studio, it would be inaccurate to state that the picture was painted from memory. The view has been identified as the one from his bedroom window, facing east, a view which Van Gogh painted variations of no fewer than twenty-one times, including The Starry Night. \"Through the iron-barred window,\" he wrote to his brother, Theo, around 23 May 1889, \"I can see an enclosed square of wheat ... above which, in the morning, I watch the sun rise in all its glory.\"",
     [2559, 2027],
-    "Starry Night",
+    "The Starry Night",
     "The_Starry_Night",
     1889
   ],
