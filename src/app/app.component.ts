9ab71351eae19681e281f22373f7ca666e2b0f57
Johnny Gérard <john@jgerard.dev>
2024-02-05 10:25:11 +0100

    feat: add slideshow page animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,58 @@
-import { trigger, transition, query, style, animate } from '@angular/animations';
+import { trigger, transition, query, style, animate, group, animation, useAnimation } from '@angular/animations';
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { ChildrenOutletContexts, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, ChildrenOutletContexts, Data, RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+
+const SLIDESHOW_TIMINGS = '.6s ease';
+const slideshowAnimation = animation([
+  style({ position: 'relative' }),
+  query(':enter, :leave', [
+    style({
+      position: 'fixed',
+      width: '100%'
+    })
+  ]),
+  group([
+    query(':leave', [
+      animate(SLIDESHOW_TIMINGS, style({ transform: 'translateX({{ leave }}100%)' }))
+    ]),
+    query(':enter', [
+      style({ transform: 'translateX({{ enter }}100%)' }),
+      animate(SLIDESHOW_TIMINGS, style({ transform: 'translateX(0%)' }))
+    ])
+  ])
+]);
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
+    AsyncPipe,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   animations: [
     trigger('routeAnimations', [
-      transition('homePage <=> slideshowPage', [
+      transition('homePage => slideshowPage', [
         query(':leave', [
-          animate('.6s ease', style({ opacity: 0 }))
+          animate('.5s ease', style({ opacity: 0 }))
         ])
+      ]),
+      transition('slideshowPage => homePage', [
+        query(':enter', [
+          style({ opacity: 0 }),
+          animate('1s ease', style({ opacity: 1 }))
+        ])
+      ])
+    ]),
+    trigger('slideshow', [
+      transition(':increment', [
+        useAnimation(slideshowAnimation, { params: { leave: '-', enter: '' } })
+      ]),
+      transition(':decrement', [
+        useAnimation(slideshowAnimation, { params: { leave: '', enter: '-' } })
       ])
     ])
   ]
@@ -23,9 +60,15 @@ import { ChildrenOutletContexts, RouterOutlet } from '@angular/router';
 export class AppComponent {
   constructor(private _nestedOutlets: ChildrenOutletContexts) { }
 
-  get routeAnimationState(): string | undefined {
-    const primary = this._nestedOutlets.getContext('primary');
+  get activatedRoute(): ActivatedRoute | null | undefined {
+    return (this._nestedOutlets.getContext('primary'))?.route;
+  }
 
-    return primary?.route?.snapshot.data['animationState'];
+  get data$(): Observable<Data> | undefined {
+    return this.activatedRoute?.data;
+  }
+
+  get routeAnimationState(): string | undefined {
+    return this.activatedRoute?.snapshot.data['animationState'];
   }
 }
