a5ee30294acc9bd76009f3f1736857456dde984c
Johnny Gérard <john@jgerard.dev>
2024-02-05 13:15:26 +0100

    feat: update title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,18 +2,21 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { paintingResolver } from './painting.resolver';
+import { slideshowTitleResolver } from './slideshow-title.resolver';
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     data: { animationState: 'homePage' },
+    title: 'galleria. | Home',
     component: HomeComponent
   },
   {
     path: 'slideshow/:slug',
     data: { animationState: 'slideshowPage' },
     resolve: { paintingIndex: paintingResolver },
+    title: slideshowTitleResolver,
     component: SlideshowComponent
   },
   {
